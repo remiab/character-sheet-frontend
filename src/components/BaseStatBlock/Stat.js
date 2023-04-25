6a2fa36db8bfa13bb35071fab152032051253f95
remiab <remi.bevan@gmail.com>
2023-04-25 17:19:51 +0100

    updated styling to position better

--- src/components/BaseStatBlock/Stat.js
+++ src/components/BaseStatBlock/Stat.js
@@ -16,12 +16,12 @@ export default function Stat(props) {
   }
 
   return (
-    <div className="Stat d-flex justify-content-center">
+    <div className="Stat d-flex justify-content-center my-3">
       <div className="box-stats">{props.dict.STAT}</div>
       <div className="box-stats d-flex justify-content-center score-row align-items-center">
-        <button className="btn adv-dis-btn">A</button>
-        <button className="btn roll-check">{props.dict.SCORE}</button>
-        <button className="btn adv-dis-btn">D</button>
+        <button className="btn col-sm-4 adv-dis-btn adv-btn">A</button>
+        <button className="btn col-sm-4 roll-check">{props.dict.SCORE}</button>
+        <button className="btn col-sm-4 adv-dis-btn dis-btn">D</button>
       </div>
       <div className="box-stats modifier-box text-align-center">{mod}</div>
       <StatDisplaySVG />
